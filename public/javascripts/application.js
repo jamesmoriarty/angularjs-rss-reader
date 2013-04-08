281d6fa89db74c6f2a3f99a7081957257bb98314
James Moriarty <jamespaulmoriarty@gmail.com>
2013-04-08 10:47:05 +1000

    feature: added history.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -29,18 +29,24 @@ config(function($routeProvider) {
 });
 
 function RssController($scope, $http) {
+  $scope.historyDisplay = 'none'
+
+  $scope.toggleHistory = function() {
+    $scope.historyDisplay === 'none' ? $scope.historyDisplay = '' : $scope.historyDisplay = 'none';
+  };
+
   $scope.rssUrls    = [];
   $scope.rssEntries = [];
 
   $scope.fetchRss = function() {
     var requestUrl = document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=JSON_CALLBACK&num=10&q=' + encodeURIComponent($scope.rssUrl);
     $http.jsonp(requestUrl).
-      success(function(data){
-        if(data.responseData != null){
+      success(function(data) {
+        if(data.responseData != null) {
           $scope.rssUrls.pushUnique($scope.rssUrl);
           var rssEntries = data.responseData.feed.entries;
           for(var n = 0; n < rssEntries.length; n++) {
-            $scope.rssEntries.pushUniqueBy(rssEntries[n], function(a, b){ return a.link === b.link; });
+            $scope.rssEntries.pushUniqueBy(rssEntries[n], function(a, b) { return a.link === b.link; });
           }
         } else {
           alert(data.responseDetails);
@@ -49,7 +55,7 @@ function RssController($scope, $http) {
   };
 }
 
-Array.prototype.pushUnique = function (obj){
+Array.prototype.pushUnique = function (obj) {
   if(this.indexOf(obj) == -1) {
     this.push(obj);
     return true;
@@ -57,7 +63,7 @@ Array.prototype.pushUnique = function (obj){
   return false;
 }
 
-Array.prototype.pushUniqueBy = function (obj, equalityFunction){
+Array.prototype.pushUniqueBy = function (obj, equalityFunction) {
   for(var n = 0; n < this.length; n++) {
     if(equalityFunction(this[n], obj)) { // contains
       return false;
@@ -66,4 +72,3 @@ Array.prototype.pushUniqueBy = function (obj, equalityFunction){
   this.push(obj);
   return true;
 }
-
